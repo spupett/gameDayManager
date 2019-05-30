28b03a1cff14edf4ea489859428c8c0e64abb4eb
Austin Williams <adwilliams@sedonagroup.com>
2019-05-30 09:49:47 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Game Day Manager</h1>
+    <user></user>
+  </div>
+</template>
+
+<script>
+
+import User from './components/User.vue'
+
+export default {
+  components: {
+    'user': User
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div id="name">
+      <label for="users">Comma seperated users</label>
+      <input type="text" name="users" id="users" v-model="userList">
+    </div>
+    <button v-on:click="showGames()">Get User</button>
+    <div id="gameids" v-bind:key=game.bggId v-for="game in sortedGames"><img v-bind:src="game.thumbnail">{{ game.name }}</div> 
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import "babel-polyfill";
+
+export default {
+  name: 'userdata',
+  data () {
+    return {
+      userList: 'spuppett, HKImpact',
+      userNames: [],
+      games: [],
+      gameIDs: []
+    }
+  },
+  methods: {
+    async showGames() {
+      this.userNames = userNames(this.userList);
+      this.gamesIDs = await allGameIDs(this.userNames);
+      this.games = await getGameDetails(this.gamesIDs);
+      console.log(this.games);
+    }
+  },
+  computed: {
+    sortedGames() {
+      return [...this.games].sort(compareGameNames);
+    }
+  }
+};
+
+function compareGameNames(g1, g2) {
+  const g1Name = g1.name.toUpperCase();
+  const g2Name = g2.name.toUpperCase();
+
+  return (g1Name > g2Name) ? 1 : -1;
+}
+
+function userNames(names) { 
+  return names.split(',').map((name) => name.trim()); 
+}
+
+async function getUsersGameList(name) {
+   return axios({
+      method: "GET",
+      "url": `http://localhost:3000/api/v1/users/${name}/games`
+    }).then((results) => {
+      return results.data;
+    })
+}
+
+async function allGameIDs(names) {
+  const allGameIDs = await Promise.all(names.map((name) => {
+    return getUsersGameList(name).then((res) => {
+        return res.map((game) => { return game.id; });      
+      });
+  }));
+
+  const combined = allGameIDs.reduce((acc, list) => {
+    return acc.concat(list);
+  }, [])
+
+  //get rid of duplicates
+  return Array.from(new Set(combined).values());
+}
+
+async function getGameDetails(games) {
+  return axios.get('http://localhost:3000/api/v1/games/', 
+    {
+      params: {
+        gameIdList: JSON.stringify(games)
+      }
+    }).then((results) => { return results.data; })
+}
+</script>
+
+
+
+<style lang="scss">
+
+</style>

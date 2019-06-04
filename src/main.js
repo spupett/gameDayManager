4a4206f87d99e442541cf6946f678cdd53dcb8dc
Austin Williams <awilliams@sedonagroup.com>
2019-06-04 10:43:34 -0500

    Routing initiated
    Routing starting.  There is a junk route, just as a placeholder.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import BootstrapVue from 'bootstrap-vue'
+import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
 
+// eslint-disable-next-line no-new
 new Vue({
-    el: '#app',
-    render: h => h(App)
+  el: '#app',
+  router,
+  render: h => h(App)
 })
